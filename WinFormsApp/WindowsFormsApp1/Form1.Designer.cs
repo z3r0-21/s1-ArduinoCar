cdcc5aa2b7642fc96f5c1bedf596af4ef3a3f3ff
Dimitar Prisadnikov <dimitarprisadnikov@studyportals.com>
2022-08-30 13:20:22 +0200

    added project files

---
+++ WinFormsApp/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class frmCar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCar));
+            this.Arduino = new System.IO.Ports.SerialPort(this.components);
+            this.btnAlaram = new System.Windows.Forms.Button();
+            this.btnNormal = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lbxBlackBox = new System.Windows.Forms.ListBox();
+            this.lblInsideTemp = new System.Windows.Forms.Label();
+            this.lblHeadlights = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblBlackBox = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Arduino
+            // 
+            this.Arduino.PortName = "COM4";
+            // 
+            // btnAlaram
+            // 
+            this.btnAlaram.BackColor = System.Drawing.Color.Red;
+            this.btnAlaram.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAlaram.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnAlaram.Location = new System.Drawing.Point(12, 12);
+            this.btnAlaram.Name = "btnAlaram";
+            this.btnAlaram.Size = new System.Drawing.Size(289, 198);
+            this.btnAlaram.TabIndex = 0;
+            this.btnAlaram.Text = "ALARM";
+            this.btnAlaram.UseVisualStyleBackColor = false;
+            this.btnAlaram.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnNormal
+            // 
+            this.btnNormal.BackColor = System.Drawing.Color.LawnGreen;
+            this.btnNormal.Location = new System.Drawing.Point(12, 216);
+            this.btnNormal.Name = "btnNormal";
+            this.btnNormal.Size = new System.Drawing.Size(289, 57);
+            this.btnNormal.TabIndex = 1;
+            this.btnNormal.Text = "Disabled alarm\r\n(back to normal mode)";
+            this.btnNormal.UseVisualStyleBackColor = false;
+            this.btnNormal.Click += new System.EventHandler(this.btnNormal_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lbxBlackBox
+            // 
+            this.lbxBlackBox.FormattingEnabled = true;
+            this.lbxBlackBox.ItemHeight = 16;
+            this.lbxBlackBox.Location = new System.Drawing.Point(421, 44);
+            this.lbxBlackBox.Name = "lbxBlackBox";
+            this.lbxBlackBox.Size = new System.Drawing.Size(201, 356);
+            this.lbxBlackBox.TabIndex = 2;
+            // 
+            // lblInsideTemp
+            // 
+            this.lblInsideTemp.AutoSize = true;
+            this.lblInsideTemp.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInsideTemp.Location = new System.Drawing.Point(7, 299);
+            this.lblInsideTemp.Name = "lblInsideTemp";
+            this.lblInsideTemp.Size = new System.Drawing.Size(306, 24);
+            this.lblInsideTemp.TabIndex = 3;
+            this.lblInsideTemp.Text = "The temperatrure inside the car is...";
+            this.lblInsideTemp.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblHeadlights
+            // 
+            this.lblHeadlights.AutoSize = true;
+            this.lblHeadlights.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeadlights.Location = new System.Drawing.Point(7, 326);
+            this.lblHeadlights.Name = "lblHeadlights";
+            this.lblHeadlights.Size = new System.Drawing.Size(200, 24);
+            this.lblHeadlights.TabIndex = 4;
+            this.lblHeadlights.Text = "Headlights status: OFF";
+            this.lblHeadlights.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.label1.Location = new System.Drawing.Point(418, 403);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(226, 46);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "The headlights status is updated every 5 seconds.";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblBlackBox
+            // 
+            this.lblBlackBox.AutoSize = true;
+            this.lblBlackBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBlackBox.Location = new System.Drawing.Point(470, 17);
+            this.lblBlackBox.Name = "lblBlackBox";
+            this.lblBlackBox.Size = new System.Drawing.Size(92, 24);
+            this.lblBlackBox.TabIndex = 6;
+            this.lblBlackBox.Text = "Black box";
+            this.lblBlackBox.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblBlackBox.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // frmCar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(647, 450);
+            this.Controls.Add(this.lblBlackBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblHeadlights);
+            this.Controls.Add(this.lblInsideTemp);
+            this.Controls.Add(this.lbxBlackBox);
+            this.Controls.Add(this.btnNormal);
+            this.Controls.Add(this.btnAlaram);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmCar";
+            this.Text = "BMW Bordcomputer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort Arduino;
+        private System.Windows.Forms.Button btnAlaram;
+        private System.Windows.Forms.Button btnNormal;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListBox lbxBlackBox;
+        private System.Windows.Forms.Label lblInsideTemp;
+        private System.Windows.Forms.Label lblHeadlights;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblBlackBox;
+    }
+}
+
---
+++ WinFormsApp/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class frmCar : Form
+    {
+        public frmCar()
+        {
+            InitializeComponent();
+            Arduino.Open();
+            timer1.Start();
+            
+            // *** *** *** IMPORTANT *** *** ***
+            //
+            //The code is working, however, if the timer frequency is set to a value lower tahn 10000ms (10 sec), the program start lagging (you can try it as well).
+            //I believe that it should be set to not less than 100ms in order to work as a real car, however, if that is the case, the buttons do not work because the program is partially frozen.
+            //By partially forzen I mean the following - the two program buttons can not be pressed, however, the blackbox, headlights status and temperature are updated just fine.
+            //If it is set to update every 10 seconds or slower, the button work, but then because of the slow updates not all temperature, black box and headlights messages are shown.
+            //
+            //I am not sure whether the issue is caused by my PC or Visual Studio, but both the Arduino and Visual Studio codes should be perfectly working.
+            //You can check that the car system is working perefectly fine by using the Arudino Serial Monitor. Type "hazard" and "normal" to simulate the two Windows form button clicks.
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Arduino.WriteLine("hazard"); //If the button is pressed it sends the message "hazard" to the Arduino.
+        }
+
+        private void btnNormal_Click(object sender, EventArgs e)
+        {   
+            Arduino.WriteLine("normal"); //If the button is pressed it sends the message "normal" to the Arduino.
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string message = Arduino.ReadLine(); //Reads the messages comming from the Arduino.
+            
+
+            if(message.Contains("C")) //Checks if the message contain the letter 'C' (only the temperature sensor message contains it).
+            {
+                lblInsideTemp.Text = $"The temperatrure inside the car is {message}."; //Display the temperature.
+            }
+            else if (message.Contains("Headlights")) //Checks if the message contain the word "Headlights".
+            {
+                if(message.Contains("OFF")) //Checks if the headlights message contain the word "OFF".
+                {
+                    lblHeadlights.Text = $"Headlights status: OFF";
+                    lbxBlackBox.Items.Add(message); //Adds the message to the black box.
+                }
+                if (message.Contains("ON")) //Checks if the headlights message contain the word "ON".
+                {
+                    lblHeadlights.Text = $"Headlights status: ON"; 
+                    lbxBlackBox.Items.Add(message); //Adds the message to the black box.
+                }
+            }
+            else
+            {
+                lbxBlackBox.Items.Add(message); //All other events messages (left/right turns, hazard/alarm mode changes) are captures and stored in the black box.
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
